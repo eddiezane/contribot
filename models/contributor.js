4e3a702fb992fc0aca721907b92bb704c8254cd9
Eddie Zaneski <eddiezane@gmail.com>
2015-04-08 22:26:51 -0400

    Add github token to contributor model

--- models/contributor.js
+++ models/contributor.js
@@ -3,7 +3,8 @@ var mongoose = require('mongoose');
 var contributorSchema = mongoose.Schema({
   _id: String,
   status_code: Number,
-  _code : { type: mongoose.Schema.ObjectId, ref: 'Code' }
+  _code : { type: mongoose.Schema.ObjectId, ref: 'Code' },
+  github_token: String
 });
 
 module.exports = mongoose.model('contributor', contributorSchema);
