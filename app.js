113e53edad37541d78e9ef4869eac18027a0fb12
Eddie Zaneski <eddiezane@gmail.com>
2015-04-09 00:53:33 -0400

    Working app flow

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ var app         = express();
 var server      = require('http').createServer(app);
 var bodyParser  = require('body-parser');
 var mongoose    = require('mongoose');
+var request     = require('request');
 var dotenv      = require('dotenv').load();
 var ghSigVerify = require('./lib/github-sig-verify.js');
+var ghRequests  = require('./lib/github-requests.js');
 var Code        = require('./models/code.js');
 var Contributor = require('./models/contributor.js');
 
@@ -12,39 +14,148 @@ mongoose.connect(process.env.MONGO_URL);
 
 app.set('port', process.env.PORT || 3000);
 
+app.use(express.static('public'));
 app.use(bodyParser.json());
 
 app.get('/', function(req, res) {
-  res.send('Hello');
+  // res.send('https://github.com/login/oauth/authorize?client_id=' + process.env.GITHUB_CLIENT_ID);
+  res.sendFile(__dirname + '/views/index.html');
 });
 
-// app.post('/webhooks/github', ghSigVerify, function(req, res) {
-app.post('/webhooks/github', function(req, res) {
+// Inbound gh webook. TODO: Abstract and clean up
+app.post('/webhooks/github', ghSigVerify, function(req, res) {
   // Only want pull requests
-  // if (req.header('X-Github-Event') !== 'pull_request') res.end();
+  if (req.header('X-Github-Event') !== 'pull_request') res.end();
 
   // We're done with the request, return 200
   res.end();
 
   // Only want merged pr's
-  if (!req.body.pull_request.merged) return;
+  if (!req.body.pull_request.merged) {
+    return console.log('pr not merged');
+  }
 
-  // TODO: Logger. Something weird happened
+  // Something weird happened
   var username = req.body.pull_request.user.login;
-  if (typeof username === 'undefined') return;
+  if (typeof username === 'undefined') {
+    return console.log('username undefined');
+  }
 
-  Contributor.findById(username, function(err, contributor) {
-    if (err) console.error(err);
+  var owner = req.body.repository.owner.login;
+  var repo  = req.body.repository.name;
 
-    if (contributor) return;
+  ghRequests.getCollaborator(owner, repo, username, function(err, statusCode) {
+    // User is a collaborator
+    if (err) {
+      return console.error(err);
+    }
 
-    Contributor.create({_id: username, status_code: 1}, function(err, newContributor) {
-      if (err) console.error(err);
+    if (statusCode == 204) {
+      return console.log('user ' + username + ' is collab');
+    }
 
-      // TODO: Reply to PR and ask them to login and oauth 
+    Contributor.findById(username, function(err, contributor) {
+      if (err) {
+        return console.error(err);
+      }
+
+      if (contributor) {
+        return console.log('contributor exists');
+      }
+
+      // User is not in db
+      Contributor.create({_id: username, status_code: 1}, function(err, newContributor) {
+        if (err) {
+          return console.error(err);
+        }
+
+        ghRequests.postComment(req.body.pull_request.comments_url, function(err, code) {
+          if (err) {
+            return console.error(err);
+          }
+
+          if (code == 201) console.log('comment posted');
+        });
+      });
     });
   });
 });
+
+app.get('/oauth/github', function(req, res) {
+  var auth_code = req.query.code;
+  ghRequests.postGithubOauth(auth_code, function(err, body) {
+    if (err) {
+      return console.error(err);
+    }
+
+    var access_token = body.access_token;
+
+    ghRequests.getUsername(access_token, function(err, resp) {
+      if (err) {
+        return console.error(err);
+      }
+
+      var user = resp.login;
+
+      Contributor.findById(user, function(err, contributor) {
+        if (err) {
+          return console.error(err);
+        }
+
+        if (!contributor) {
+          return console.log('contributor not found during oauth');
+        }
+
+        // They don't belong here
+        if (contributor.status_code !== 1) {
+          // FIXME: Might we wrong http code
+          return res.redirect(303, '/redeem?access_token=' + access_token);
+        }
+
+        Code.findOne({used: false}, function(err, pf_code) {
+          contributor.github_token = access_token;
+          contributor._code        = pf_code._id;
+          contributor.status_code  = 2;
+
+          contributor.save(function(err) {
+            if (err) {
+              return console.error(err);
+            }
+
+            pf_code.used = true;
+
+            pf_code.save(function(err) {
+              if (err) {
+                return console.error(err);
+              }
+
+              return res.redirect(307, pf_code._id);
+            });
+
+          });
+        });
+      });
+    });
+  });
+});
+
+app.get('/redeem', function(req, res) {
+  var access_token = req.query.access_token;
+  Contributor.findOne({github_token: access_token}, function(err, contributor) {
+    if (err) {
+      console.error(err);
+      res.send('bad');
+    }
+
+    var statusCode = contributor.status_code;
+    if (statusCode !== 2 && statusCode !== 3) {
+      return res.redirect(307, '/');
+    }
+
+    res.redirect(307, contributor._code);
+  });
+});
+
 
 server.listen(app.get('port'), function() {
   console.log('Running on port %s', app.get('port'));
--- models/contributor.js
+++ models/contributor.js
@@ -3,7 +3,7 @@ var mongoose = require('mongoose');
 var contributorSchema = mongoose.Schema({
   _id: String,
   status_code: Number,
-  _code : { type: mongoose.Schema.ObjectId, ref: 'Code' },
+  _code : { type: String, ref: 'Code' },
   github_token: String
 });
 
